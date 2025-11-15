fcbca51b1bcec299c095c2581ec6e1957a49349c
Daniel Purucker <mail@daniel-purucker.com>
2025-11-15 07:50:19 +0100

    reorder

--- bootstrap_system.sh
+++ bootstrap_system.sh
@@ -3,6 +3,11 @@
 # see: https://wiki.archlinux.org/title/Installation_guide#
 # https://archinstall.archlinux.page/installing/guided.html
 
+# Base URL for configuration files
+BASE_URL="https://raw.githubusercontent.com/dapuru/archinstall/refs/heads/main"
+REPO_URL="https://github.com/dapuru/archinstall.git"
+DOT_URL="https://github.com/dapuru/dotfiles.git"
+
 echo "Setting defaults..."
 # loadkeys DE
 loadkeys de-latin1
@@ -14,26 +19,34 @@ timedatectl set-timezone Europe/Berlin
 timedatectl set-ntp true
 
 # add vim and wget
-sudo pacman -Sy vim 
+#sudo pacman -Sy vim 
 
 # Getting installation type
 
 echo "Enter installation type (server, desktop, vm): " && read installation_type
 
-if [ "$installation_type" == "server" ]; then
-    wget https://raw.githubusercontent.com/dapuru/archinstall/refs/heads/main/user_configuration_server.json
-    conf_name="user_configuration_server.json"
-elif [ "$installation_type" == "desktop" ]; then
-    wget https://raw.githubusercontent.com/dapuru/archinstall/refs/heads/main/user_configuration_desktop.json
-    conf_name="user_configuration_desktop.json"
-elif [ "$installation_type" == "vm" ]; then
-    wget https://raw.githubusercontent.com/dapuru/archinstall/refs/heads/main/user_configuration_vm.json
-    conf_name="user_configuration_vm.json"
-else
-    echo "Invalid installation type. Exiting..."
+# Determine configuration file URL based on installation type
+case "$installation_type" in
+    server)
+        config_url="${BASE_URL}/user_configuration_server.json"
+        ;;
+    desktop)
+        config_url="${BASE_URL}/user_configuration_desktop.json"
+        ;;
+    vm)
+        config_url="${BASE_URL}/user_configuration_vm.json"
+        ;;
+    *)
+        echo "Invalid installation type. Exiting..."
+        exit 1
+        ;;
+esac
+
+# Download configuration with integrity check
+if ! wget "$config_url" -O "user_config.json"; then
+    log "Failed to download configuration."
     exit 1
 fi
-
 
 # get config-files
 echo "Using $installation_type ..."
@@ -48,41 +61,37 @@ fi
 #echo "Enter hostname: "
 #read hostname
 
-# get credentials
-echo "Setting up credentials..."
-echo "Enter root password: "
-read -s root_password
-echo "Re-enter root password: "
-read -s re_root_password
+# Function to get matching passwords
+get_password() {
+    while true; do
+        echo "$1"
+        read -s password
+        echo "Re-enter password: "
+        read -s re_password
+        if [ "$password" == "$re_password" ]; then
+            echo "$password"
+            return
+        else
+            echo "Passwords do not match. Please try again."
+        fi
+    done
+}
 
-if [ "$root_password" != "$re_root_password" ]; then
-	echo "Passwords do not match. Exiting..."
-	exit 1
-fi
+root_password=$(get_password "Enter root password: ")
 
-echo "Enter username: "
-read username
-echo "Enter password: "
-read -s password
-echo "Re-enter password: "
-read -s re_password	
-# Compare passwords
+username=""
+while true; do
+    echo "Enter username (no spaces): "
+    read username
+    if [[ "$username" =~ ^[a-zA-Z0-9_]+$ ]]; then
+        break
+    else
+        echo "Invalid username. Please try again."
+    fi
+done
 
-if [ "$password" != "$re_password" ]; then
-	echo "Passwords do not match. Exiting..."
-	exit 1
-fi
-
-# Enter Encryption Password
-echo "Enter encryption password: "
-read -s encryption_password
-echo "Re-enter encryption password: "
-read -s re_encryption_password
-
-if [ "$encryption_password" != "$re_encryption_password" ]; then
-	echo "Passwords do not match. Exiting..."
-	exit 1
-fi
+password=$(get_password "Enter password: ")
+encryption_password=$(get_password "Enter encryption password: ")
 
 # write config
 echo "Writing UserConfig..."
@@ -97,11 +106,70 @@ cat << EOF > user_credentials.json
             "username": "${username}"
         }
     ],
-    "!encryption_password": "${encryption_password}"
+    "encryption_password": "${encryption_password}"
 }
 EOF
 
-# Start archinstall
-archinstall --config $(conf_name) --creds user_credentials.json
 
+# Start archinstall
+if ! archinstall --config "user_config.json" --creds user_credentials.json; then
+    log "Failed to run archinstall."
+    exit 1
+fi
+
+echo "*************** Installation completed successfully. Starting re-works **************"
+
+arch-chroot /mnt
+
+# clone complete repos
+git clone $REPO_URL /home/$USER/archinstall
+git clone $DOT_URL /home/$USER/dotfiles
+
+# install packages
+echo "Do you want to install packages? (y/n)"
+read -r install_packages    
+if [ "$install_packages" == "y" ]; then
+	echo "Installing packages..."
+	pacman -S --needed - < /home/$USER/archinstall/pkglist.txt
+fi
+
+# install dotfiles
+echo "Do you want to install dotfiles? (y/n)"
+read -r install_dotfiles
+if [ "$install_dotfiles" == "y" ]; then
+	echo "Installing dotfiles..."
+	cd /home/$USER/dotfiles
+	stow
+fi
+
+# restore home?
+echo "Do you want to restore your home? (y/n)"
+read -r restore_home
+if [ "$restore_home" == "y" ]; then
+	echo "Restoring home..."
+
+	# TODO
+	echo "Will home be (1) btrfs or (2) zfs?"
+	read -r home_type
+	if [ "$home_type" == "1" ]; then
+		echo "Your home is btrfs"
+	elif [ "$home_type" == "2" ]; then
+		echo "Please proceed manually, if you really want to... Aborting".
+		exit 1
+	fi
+
+	echo "Restore from (1) local server, (2) remote server, (3) USB: "
+	read -r restore_from
+	# TODO
+fi
+
+
+
+# Do you want to reboot?
+echo "Do you want to reboot? (y/n)"
+read -r reboot
+if [ "$reboot" == "y" ]; then
+	echo "Rebooting..."
+	reboot
+fi
 
